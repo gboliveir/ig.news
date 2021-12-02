5aa33d715ad31310c902c88400d085f7cb0423a9
Gabriel Brito <gabrielbrito.dev@gmail.com>
2021-12-01 23:20:22 -0300

    feat: files for configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head  from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news | Home Page</title>
+      </Head>
+      <h1>
+        Hello World
+      </h1>
+    </>
+  );
+}
