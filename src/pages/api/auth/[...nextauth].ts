0e582c71104b90b4580e8a8865ca4b91910090f8
Gabriel Brito Oliveira <gabriel.brito@neogrid.com>
2021-12-24 18:33:20 -0300

    feat: configuration faunadb and connection users collection

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
@@ -11,4 +14,37 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try { 
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        );
+        return true
+      } catch {
+        return false;
+      }
+    },
+  }
 })
